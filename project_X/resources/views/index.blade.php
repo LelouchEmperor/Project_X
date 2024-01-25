733483f7126913eb159a366469c2a597e2a5c50b
Alex22JAMES <91525156+Alex22JAMES@users.noreply.github.com>
2024-01-25 23:41:21 +0100

    Debut du projet X

---
+++ project_X/resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="chat">
+        <div class="top">
+        </div>
+
+        <div class="messages">
+
+        </div>
+
+        <div class="bottom">
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ project_X/resources/views/receive.blade.php
@@ -0,0 +1,15 @@
+<div class="left message">
+    <div class="message__header">
+        <div class="message__header__title">
+            <h3>Message</h3>
+        </div>
+        <div class="message__header__close">
+            <i class="fas fa-times"></i>
+        </div>
+    </div>
+    <div class="message__body">
+        <div class="message__body__content">
+            <p>{{ $message }}</p>
+        </div>
+    </div>
+</div>
